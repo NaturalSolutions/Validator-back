177d3109cfabda3490d8197b7a94b216e555f4de
SThomaty <sandiethomaty@gmail.com>
2017-03-19 20:21:29 +0100

    add of setval of users_seq_id

--- MCD/sql_insert/output/users.sql
+++ MCD/sql_insert/output/users.sql
@@ -1,3 +1,4 @@
-INSERT INTO public.users(
-	id, lastname, firstname, email, picture, categories_id, accounts_id)
-	VALUES (1, 'Tom', 'Trucani', 'user1@mymail.org', '', 3, 1);
+INSERT INTO public.users(id, lastname, firstname, email, picture, categories_id, accounts_id)VALUES (1, 'Tom', 'Trucani','user1@mymail.org', '', 3, 1);
+
+SELECT setval('users_id_seq',(SELECT max(id) FROM public.users));
+
